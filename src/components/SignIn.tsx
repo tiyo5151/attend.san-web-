bec20136de1b976835edf25a1cedc93beab530fd
skmtrd <skmtgame23@gmail.com>
2024-09-04 09:53:45 +0900

    feat: logout config, display session info

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -3,6 +3,7 @@ import { signIn } from '../../auth';
 const SignIn = () => {
   return (
     <form
+      className='rounded-xl p-3 ring-1 ring-slate-400'
       action={async () => {
         'use server';
         await signIn('google');
