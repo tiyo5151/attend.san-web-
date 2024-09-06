c5bc037da01dc19f073cfc15425bf4a85e41569a
skmtrd <skmtgame23@gmail.com>
2024-09-06 19:14:58 +0900

    chore: auth.ts

--- auth.ts
+++ auth.ts
@@ -6,4 +6,5 @@ import Google from 'next-auth/providers/google';
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
+  secret: process.env.NEXTAUTH_SECRET,
 });
