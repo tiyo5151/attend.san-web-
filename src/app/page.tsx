710728377172888e76c7a2e879d4917e871174f9
tiyo5151 <s1F102400224@iniad.org>
2024-12-30 22:58:41 +0900

    page.tsx修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 import SignIn from '@/components/SignIn';
 import SignOut from '@/components/SignOut';
-import Image from 'next/image';
 import { auth } from '../../auth';
 
 const Home = async () => {
   const session = await auth();
+  if (!session?.user?.image) return <SignIn />;
 
   return (
-    <div className='flex h-screen w-full flex-col items-center justify-center gap-y-10'>
-      <div className='flex'>
-        {!session?.user?.image ? (
-          <SignIn />
-        ) : (
-          <div className='flex gap-4'>
-            <SignOut />
-            <Image
-              src={session.user.image}
-              style={{ borderRadius: '50%', objectFit: 'cover' }}
-              alt='image of user'
-              width={60}
-              height={20}
-            ></Image>
-          </div>
-        )}
-      </div>
+    <div className='flex h-screen w-full flex-col items-center justify-center'>
+      <SignOut />
     </div>
   );
 };
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -2,15 +2,17 @@ import { signIn } from '../../auth';
 
 const SignIn = () => {
   return (
-    <form
-      className='rounded-xl p-3 ring-1 ring-slate-400'
-      action={async () => {
-        'use server';
-        await signIn('google');
-      }}
-    >
-      <button type='submit'>Signin with Google</button>
-    </form>
+    <div className='flex h-screen w-full flex-col items-center justify-center'>
+      <form
+        className='rounded-xl p-3 ring-1 ring-slate-400'
+        action={async () => {
+          'use server';
+          await signIn('google');
+        }}
+      >
+        <button type='submit'>Signin with Google</button>
+      </form>
+    </div>
   );
 };
 
