95e631319cd584c2673923c6a8de35664a421f30
riku546 <opa9788@gmail.com>
2025-03-13 00:04:51 +0900

    でもデータ削除

--- src/app/api/timetable/register/route.ts
+++ src/app/api/timetable/register/route.ts
@@ -3,17 +3,13 @@ import { handleAPIError } from '@/lib/handleAPIError';
 import { prisma } from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
-type TimeTable = {
-  [periodNumber: number]: string[];
-};
+type TimeTable = { [periodNumber: number]: string[] };
 
 export const POST = async (req: NextRequest) =>
   handleAPIError(async () => {
-    // const { timeTableJsonText } = await req.json();
+    const { timeTableJsonText } = await req.json();
 
-    //デモのjsonデータ
-    const timeTableJsonText =
-      '{"1":["","","","","リスニング・スピーキング演習II/リスニング・スピーキング演習II(英語)/リスニング・スピーキング演習II(日本語)英語12",""],"2":["","リスニング・スピーキング演習II/リスニング・スピーキング演習II(英語)/リスニング・スピーキング演習II(日本語)英語12","情報連携学概論II","リーディング・ライティング演習II/リーディング・ライティング演習II(英語)/リーディング・ライティング演習II(日本語)英語7","",""],"3":["","","情報連携基礎実習II/情報連携実習IB3","","",""],"4":["","","","コンピュータ・サイエンス概論II4","情報連携のための数学A/情報連携のための数学I2",""],"5":["コンピュータ・サイエンス概論II4","","","","コンピュータ・サイエンス基礎演習II/情報連携基礎演習II7",""],"6":["","","マクロ経済学日本語","","",""],"7":["","","","","",""],"8":["","","","","",""]}';
+
 
     const objectTimeTable = JSON.parse(timeTableJsonText) as TimeTable;
 
@@ -47,11 +43,7 @@ const registerLectureFacade = async (
 };
 
 const checkLectureExist = async (lectureName: string) => {
-  const lecture = await prisma.lecture.findFirst({
-    where: {
-      name: lectureName,
-    },
-  });
+  const lecture = await prisma.lecture.findFirst({ where: { name: lectureName } });
 
   const isExist = lecture !== null;
 
@@ -59,21 +51,13 @@ const checkLectureExist = async (lectureName: string) => {
 };
 
 const registerLecture = async (lectureName: string) => {
-  const lecture = await prisma.lecture.create({
-    data: {
-      name: lectureName,
-    },
-  });
+  const lecture = await prisma.lecture.create({ data: { name: lectureName } });
 
   return lecture;
 };
 
 const getLectureId = async (lectureName: string) => {
-  const lecture = await prisma.lecture.findFirst({
-    where: {
-      name: lectureName,
-    },
-  });
+  const lecture = await prisma.lecture.findFirst({ where: { name: lectureName } });
 
   return lecture?.id;
 };
